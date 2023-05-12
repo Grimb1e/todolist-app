b8e3301805a7893056c54d62c66ad8411b4e3997
unknown <batu73580@gmail.com>
2023-05-12 19:43:37 +0400

    todo app:

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const todos = [];
+let todos = [];
 
 const form = document.querySelector("form");
 const input = document.querySelector("input");
@@ -7,22 +7,31 @@ const todoList = document.getElementById("todo-list");
 
 const formHandler = (e) => {
   e.preventDefault();
-  todos.push({
+  todos.unshift({
     id: new Date().getTime().toString(),
     text: input.value,
   });
-  console.log(input.value);
   input.value = "";
   renderTools();
 };
+
+const deleteTodo = (id) => {
+  todos = todos.filter(todo => todo.id !== id);
+  renderTools();
+}
 
 const renderTools = () => {
   todoList.innerHTML = "";
   todos.forEach((todo) => {
     const todoItem = document.createElement("li");
     todoItem.innerHTML = todo.text;
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete";
+    deleteButton.addEventListener("click", () => deleteTodo(todo.id));
+    todoItem.appendChild(deleteButton);
     todoList.appendChild(todoItem);
   });
 };
+
 
 form.addEventListener("submit", formHandler);
